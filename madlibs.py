b5bc9dafad69dbf2e28f9e5b151183eb31db943f
Rachel <radonwrites@gmail.com>
2019-09-18 19:18:25 -0400

    updated

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,5 @@
-print("hello world")
+import random
+verbs = ["run","skip","frolic","hide","duck","swim"]
+nouns = ["The cat","An orange"]
+print(verbs)
+print(random.choice(nouns) + " decided to " + random.choice(verbs))
