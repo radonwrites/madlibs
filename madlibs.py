3dacaa588e7c9a8386452ef1312aabe91f827667
Mark Bennett <mark.bennett@ucf.edu>
2019-09-18 19:32:09 -0400

    updated nouns and added obj

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,9 @@
+#%%
 import random
 verbs = ["run","skip","frolic","hide","duck","swim"]
-nouns = ["The cat","An orange"]
-print(verbs)
-print(random.choice(nouns) + " decided to " + random.choice(verbs))
+nouns = ["The cat","An orange","The ball","The boy","The girl","A wolf"]
+obj = ["bench","hurdle","paddle","wall"]
+
+print(random.choice(nouns) + " decided to " + random.choice(verbs) + " over the " + random.choice(obj))
+
+#%%
